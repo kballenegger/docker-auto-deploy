08b1d2f6ff06fb24cbb3e6460b025c1cb353ea8d
Kenneth Ballenegger <kenneth@ballenegger.com>
2014-02-12 23:54:14 -0800

    run script from conf, email out results

--- app.rb
+++ app.rb
@@ -1,4 +1,9 @@
 
+require 'yaml'
+require 'mail'
+
+# Assumes configuration is present on the filesystem at path:
+#  - /auto-deploy-conf.yml
 
 conf_path = '/auto-deploy-conf.yml'
 Conf = YAML.load_file(conf_path)
@@ -6,12 +11,27 @@ Conf = YAML.load_file(conf_path)
 class AutoDeployController < Kenji::Controller
   post '/:secret/deploy' do |secret|
     unless secret == Conf['secret']
-      puts 'deploy request unauthorized'
+      puts 'Bad authentication.'
       kenji.respond(403, 'Bad authentication.')
     end
-    puts 'deploy request unauthorized... processing'
 
 
-    # TODO: implement me
+    log = 'Deploy request authorized... processing.'
+    log << "\n\n---\n\n"
+
+    script = Conf['sript']
+
+    # restart app...
+
+    log << `#{script}`
+    log << "\n\n."
+
+    mail = Mail.new do
+      subject 'Quoth deploy...'
+      to Conf['mailto']
+      body log
+    end
+    mail.delivery_method :smtp, Conf['smtp']
+    mail.deliver
   end
 end
